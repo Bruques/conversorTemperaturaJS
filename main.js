f266112fa5a875d8512bd6748a301dd1b18f343d
Bruques <89554226+Bruques@users.noreply.github.com>
2021-12-17 04:51:15 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let resp = window.document.getElementById('resultado')
+let resp2 = window.document.getElementById('resultado2')
+
+function converter1() {
+  let celsius = window.prompt('Digite uma temperatura em ºC (Celsius)')
+  let convert1 = parseFloat(celsius * 1.8 + 32).toFixed(2)
+  resp.innerHTML = `<h1>A temperatura de ${celsius}, corresponde a...</h1>`
+  resp2.innerHTML = `<p>${convert1} ºF (Fahrenheit)</p>`
+}
+
+function converter2() {
+  let fahrenheit = window.prompt('Digite uma temperatura em ºF (Fahrenheit)')
+  let convert2 = parseFloat((fahrenheit - 32) / 1.8).toFixed(2)
+  resp.innerHTML = `<h1>A temperatura de ${fahrenheit}, corresponde a...</h1>`
+  resp2.innerHTML = `<p>${convert2} ºC (Celsius)</p>`
+}
